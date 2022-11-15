8b1f26df3b94b3131b19fcce06e7aed9b7d87de4
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-11-14 21:13:10 -0600

    work on Footer

--- homework/src/App.js
+++ homework/src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Contact from './components/Contact/Contact'
 import Work from './components/Work/Work'
 import Resume from './components/Resume/Resume.js'
+import Footer from './components/Footer/Footer.js'
 
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -12,8 +13,9 @@ function App() {
   return (
     <>
     <CssBaseline />
-    <NavBar />;
-    <Resume />
+    <NavBar />
+    <About />
+    <Footer />
     </>
   )
 }
