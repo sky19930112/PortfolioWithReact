ea4a9b3bf245482b1f8103ea4b10c05417d2c8d1
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-11-14 21:04:27 -0600

    work on resume page

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import NavBar from './components/Nav/Nav';
 import About from './components/About'
 import Contact from './components/Contact/Contact'
 import Work from './components/Work/Work'
+import Resume from './components/Resume/Resume.js'
+
 import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
@@ -10,7 +13,7 @@ function App() {
     <>
     <CssBaseline />
     <NavBar />;
-    <Work />
+    <Resume />
     </>
   )
 }
